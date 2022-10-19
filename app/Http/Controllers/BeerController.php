aa888910de8ea41a5ff9e3bc976e70910ac4048e
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-10-19 14:18:24 -0300

    Trazendo a lista de collection que será exportada

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\BeerExport;
+use App\Http\Requests\BeerRequest;
 use App\Services\PunkApiService;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class BeerController extends Controller
 {
@@ -11,9 +14,24 @@ class BeerController extends Controller
     public function __construct(protected PunkApiService $service)
     { }
 
-    public function index(Request $request)
+    public function index(BeerRequest $request)
     {
         return $this->service
-                    ->getBeers(...$request->all());
+                    ->getBeers(...$request->validated());
+    }
+
+    public function export(BeerRequest $request)
+    {
+        $beers = $this->service->getBeers(...$request->validated());
+
+        $filteredBeers = collect($beers)->map( function ($value) {
+            return collect($value)
+                ->only(['name', 'tagline', 'first_brewed', 'description'])
+                ->toArray();
+        })->toArray();
+
+        Excel::store(new BeerExport($filteredBeers), 'olw-report.xlsx', 's3');
+
+        return 'criado';
     }
 }
